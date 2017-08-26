43d80d7de5c34ba4bee1c99beaf202723ddd43a5
Bogdan Chadkin <trysound@yandex.ru>
2017-08-26 04:54:52 +0300

    feat(types): Specify event types for each event function (#134)
    * Specify event types for each event function
    
    * Specify ErrorEvent
    
    * Specify HashChangeEvent and PopStateEvent

--- type-definitions/dom-event.d.ts
+++ type-definitions/dom-event.d.ts
@@ -2,33 +2,33 @@ import {Stream} from "most";
 
 export function domEvent(event: string, node: EventTarget, capture?: boolean): Stream<Event>
 
-export function blur(node: EventTarget, capture?: boolean): Stream<Event>;
-export function focus(node: EventTarget, capture?: boolean): Stream<Event>;
-export function focusin(node: EventTarget, capture?: boolean): Stream<Event>;
-export function focusout(node: EventTarget, capture?: boolean): Stream<Event>;
-export function click(node: EventTarget, capture?: boolean): Stream<Event>;
-export function dblclick(node: EventTarget, capture?: boolean): Stream<Event>;
-export function mousedown(node: EventTarget, capture?: boolean): Stream<Event>;
-export function mouseup(node: EventTarget, capture?: boolean): Stream<Event>;
-export function mousemove(node: EventTarget, capture?: boolean): Stream<Event>;
-export function mouseover(node: EventTarget, capture?: boolean): Stream<Event>;
-export function mouseenter(node: EventTarget, capture?: boolean): Stream<Event>;
-export function mouseout(node: EventTarget, capture?: boolean): Stream<Event>;
-export function mouseleave(node: EventTarget, capture?: boolean): Stream<Event>;
+export function blur(node: EventTarget, capture?: boolean): Stream<FocusEvent>;
+export function focus(node: EventTarget, capture?: boolean): Stream<FocusEvent>;
+export function focusin(node: EventTarget, capture?: boolean): Stream<FocusEvent>;
+export function focusout(node: EventTarget, capture?: boolean): Stream<FocusEvent>;
+export function click(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
+export function dblclick(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
+export function mousedown(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
+export function mouseup(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
+export function mousemove(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
+export function mouseover(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
+export function mouseenter(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
+export function mouseout(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
+export function mouseleave(node: EventTarget, capture?: boolean): Stream<MouseEvent>;
 export function change(node: EventTarget, capture?: boolean): Stream<Event>;
-export function select(node: EventTarget, capture?: boolean): Stream<Event>;
+export function select(node: EventTarget, capture?: boolean): Stream<UIEvent>;
 export function submit(node: EventTarget, capture?: boolean): Stream<Event>;
-export function keydown(node: EventTarget, capture?: boolean): Stream<Event>;
-export function keypress(node: EventTarget, capture?: boolean): Stream<Event>;
-export function keyup(node: EventTarget, capture?: boolean): Stream<Event>;
+export function keydown(node: EventTarget, capture?: boolean): Stream<KeyboardEvent>;
+export function keypress(node: EventTarget, capture?: boolean): Stream<KeyboardEvent>;
+export function keyup(node: EventTarget, capture?: boolean): Stream<KeyboardEvent>;
 export function input(node: EventTarget, capture?: boolean): Stream<Event>;
-export function contextmenu(node: EventTarget, capture?: boolean): Stream<Event>;
-export function resize(node: EventTarget, capture?: boolean): Stream<Event>;
-export function scroll(node: EventTarget, capture?: boolean): Stream<Event>;
-export function error(node: EventTarget, capture?: boolean): Stream<Event>;
+export function contextmenu(node: EventTarget, capture?: boolean): Stream<PointerEvent>;
+export function resize(node: EventTarget, capture?: boolean): Stream<UIEvent>;
+export function scroll(node: EventTarget, capture?: boolean): Stream<UIEvent>;
+export function error(node: EventTarget, capture?: boolean): Stream<ErrorEvent>;
 
-export function hashchange(node: EventTarget, capture?: boolean): Stream<Event>;
-export function popstate(node: EventTarget, capture?: boolean): Stream<Event>;
+export function hashchange(node: EventTarget, capture?: boolean): Stream<HashChangeEvent>;
+export function popstate(node: EventTarget, capture?: boolean): Stream<PopStateEvent>;
 export function load(node: EventTarget, capture?: boolean): Stream<Event>;
 export function unload(node: EventTarget, capture?: boolean): Stream<Event>;
 
@@ -40,7 +40,7 @@ export function pointerenter(node: EventTarget, capture?: boolean): Stream<Event
 export function pointerout(node: EventTarget, capture?: boolean): Stream<Event>;
 export function pointerleave(node: EventTarget, capture?: boolean): Stream<Event>;
 
-export function touchstart(node: EventTarget, capture?: boolean): Stream<Event>;
-export function touchend(node: EventTarget, capture?: boolean): Stream<Event>;
-export function touchmove(node: EventTarget, capture?: boolean): Stream<Event>;
-export function touchcancel(node: EventTarget, capture?: boolean): Stream<Event>;
+export function touchstart(node: EventTarget, capture?: boolean): Stream<TouchEvent>;
+export function touchend(node: EventTarget, capture?: boolean): Stream<TouchEvent>;
+export function touchmove(node: EventTarget, capture?: boolean): Stream<TouchEvent>;
+export function touchcancel(node: EventTarget, capture?: boolean): Stream<TouchEvent>;
